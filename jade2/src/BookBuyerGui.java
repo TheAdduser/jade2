b37b567666d3ebc09ec69581f86f17e25f39abad
TheAdduser <EasyRider190@gmail.com>
2024-05-15 11:57:59 +0200

    Zad 2 finish

--- jade2/src/BookBuyerGui.java
+++ jade2/src/BookBuyerGui.java
@@ -6,23 +6,23 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-class BookBuyerGui extends JFrame {	
+class BookBuyerGui extends JFrame {
 	private BookBuyerAgent myAgent;
-	
+
 	private JTextField titleField;
-	
+
 	BookBuyerGui(BookBuyerAgent a) {
 		super(a.getLocalName());
-		
+
 		myAgent = a;
-		
+
 		JPanel p = new JPanel();
 		p.setLayout(new GridLayout(2, 2));
 		p.add(new JLabel("Title:"));
 		titleField = new JTextField(15);
 		p.add(titleField);
 		getContentPane().add(p, BorderLayout.CENTER);
-		
+
 		JButton addButton = new JButton("Search");
 		addButton.addActionListener( new ActionListener() {
 			public void actionPerformed(ActionEvent ev) {
@@ -32,23 +32,23 @@ class BookBuyerGui extends JFrame {
 					titleField.setText("");
 				}
 				catch (Exception e) {
-					JOptionPane.showMessageDialog(BookBuyerGui.this, "Invalid values. " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE); 
+					JOptionPane.showMessageDialog(BookBuyerGui.this, "Invalid values. " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
 				}
 			}
 		} );
 		p = new JPanel();
 		p.add(addButton);
 		getContentPane().add(p, BorderLayout.SOUTH);
-		
+
 		addWindowListener(new	WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
 				myAgent.doDelete();
 			}
 		} );
-		
+
 		setResizable(false);
 	}
-	
+
 	public void display() {
 		pack();
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -56,5 +56,5 @@ class BookBuyerGui extends JFrame {
 		int centerY = (int)screenSize.getHeight() / 2;
 		setLocation(centerX - getWidth() / 2, centerY - getHeight() / 2);
 		setVisible(true);
-	}	
+	}
 }
--- jade2/src/BookSellerAgent.java
+++ jade2/src/BookSellerAgent.java
@@ -12,107 +12,107 @@ import jade.domain.FIPAAgentManagement.ServiceDescription;
 import java.util.*;
 
 public class BookSellerAgent extends Agent {
-  private Hashtable catalogue;
-  private BookSellerGui myGui;
+	private Hashtable catalogue;
+	private BookSellerGui myGui;
 
-  protected void setup() {
-    catalogue = new Hashtable();
-    myGui = new BookSellerGui(this);
-    myGui.display();
+	protected void setup() {
+		catalogue = new Hashtable();
+		myGui = new BookSellerGui(this);
+		myGui.display();
 
-    //book selling service registration at DF
-    DFAgentDescription dfd = new DFAgentDescription();
-    dfd.setName(getAID());
-    ServiceDescription sd = new ServiceDescription();
-    sd.setType("book-selling");
-    sd.setName("JADE-book-trading");
-    dfd.addServices(sd);
-    try {
-      DFService.register(this, dfd);
-    }
-    catch (FIPAException fe) {
-      fe.printStackTrace();
-    }
-    
-    addBehaviour(new OfferRequestsServer());
+		//book selling service registration at DF
+		DFAgentDescription dfd = new DFAgentDescription();
+		dfd.setName(getAID());
+		ServiceDescription sd = new ServiceDescription();
+		sd.setType("book-selling");
+		sd.setName("JADE-book-trading");
+		dfd.addServices(sd);
+		try {
+			DFService.register(this, dfd);
+		}
+		catch (FIPAException fe) {
+			fe.printStackTrace();
+		}
 
-    addBehaviour(new PurchaseOrdersServer());
-  }
+		addBehaviour(new OfferRequestsServer());
 
-  protected void takeDown() {
-    //book selling service deregistration at DF
-    try {
-      DFService.deregister(this);
-    }
-    catch (FIPAException fe) {
-      fe.printStackTrace();
-    }
-  	myGui.dispose();
-    System.out.println("Seller agent " + getAID().getName() + " terminated.");
-  }
-
-  //invoked from GUI, when a new book is added to the catalogue
-  public void updateCatalogue(final String title, final int price) {
-    addBehaviour(new OneShotBehaviour() {
-      public void action() {
-		catalogue.put(title, new Integer(price));
-		System.out.println(getAID().getLocalName() + ": " + title + " put into the catalogue. Price = " + price);
-      }
-    } );
-  }
-  
-	private class OfferRequestsServer extends CyclicBehaviour {
-	  public void action() {
-	    //proposals only template
-		MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.CFP);
-		ACLMessage msg = myAgent.receive(mt);
-	    if (msg != null) {
-	      String title = msg.getContent();
-	      ACLMessage reply = msg.createReply();
-	      Integer price = (Integer) catalogue.get(title);
-	      if (price != null) {
-	        //title found in the catalogue, respond with its price as a proposal
-	        reply.setPerformative(ACLMessage.PROPOSE);
-	        reply.setContent(String.valueOf(price.intValue()));
-	      }
-	      else {
-	        //title not found in the catalogue
-	        reply.setPerformative(ACLMessage.REFUSE);
-	        reply.setContent("not-available");
-	      }
-	      myAgent.send(reply);
-	    }
-	    else {
-	      block();
-	    }
-	  }
+		addBehaviour(new PurchaseOrdersServer());
 	}
 
-	
-	private class PurchaseOrdersServer extends CyclicBehaviour {
-	  public void action() {
-	    //purchase order as proposal acceptance only template
-		MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.ACCEPT_PROPOSAL);
-		ACLMessage msg = myAgent.receive(mt);
-	    if (msg != null) {
-	      String title = msg.getContent();
-	      ACLMessage reply = msg.createReply();
-	      Integer price = (Integer) catalogue.remove(title);
-	      if (price != null) {
-	        reply.setPerformative(ACLMessage.INFORM);
-	        System.out.println(getAID().getLocalName() + ": " + title + " sold to " + msg.getSender().getLocalName());
-	      }
-	      else {
-	        //title not found in the catalogue, sold to another agent in the meantime (after proposal submission)
-	        reply.setPerformative(ACLMessage.FAILURE);
-	        reply.setContent("not-available");
-	      }
-	      myAgent.send(reply);
-	    }
-	    else {
-		  block();
+	protected void takeDown() {
+		//book selling service deregistration at DF
+		try {
+			DFService.deregister(this);
 		}
-	  }
+		catch (FIPAException fe) {
+			fe.printStackTrace();
+		}
+		myGui.dispose();
+		System.out.println("Seller agent " + getAID().getName() + " terminated.");
+	}
+
+	//invoked from GUI, when a new book is added to the catalogue
+	public void updateCatalogue(final String title, final int price) {
+		addBehaviour(new OneShotBehaviour() {
+			public void action() {
+				catalogue.put(title, new Integer(price));
+				System.out.println(getAID().getLocalName() + ": " + title + " put into the catalogue. Price = " + price);
+			}
+		} );
+	}
+
+	private class OfferRequestsServer extends CyclicBehaviour {
+		public void action() {
+			//proposals only template
+			MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.CFP);
+			ACLMessage msg = myAgent.receive(mt);
+			if (msg != null) {
+				String title = msg.getContent();
+				ACLMessage reply = msg.createReply();
+				Integer price = (Integer) catalogue.get(title);
+				if (price != null) {
+					//title found in the catalogue, respond with its price as a proposal
+					reply.setPerformative(ACLMessage.PROPOSE);
+					reply.setContent(String.valueOf(price.intValue()));
+				}
+				else {
+					//title not found in the catalogue
+					reply.setPerformative(ACLMessage.REFUSE);
+					reply.setContent("not-available");
+				}
+				myAgent.send(reply);
+			}
+			else {
+				block();
+			}
+		}
+	}
+
+
+	private class PurchaseOrdersServer extends CyclicBehaviour {
+		public void action() {
+			//purchase order as proposal acceptance only template
+			MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.ACCEPT_PROPOSAL);
+			ACLMessage msg = myAgent.receive(mt);
+			if (msg != null) {
+				String title = msg.getContent();
+				ACLMessage reply = msg.createReply();
+				Integer price = (Integer) catalogue.remove(title);
+				if (price != null) {
+					reply.setPerformative(ACLMessage.INFORM);
+					System.out.println(getAID().getLocalName() + ": " + title + " sold to " + msg.getSender().getLocalName());
+				}
+				else {
+					//title not found in the catalogue, sold to another agent in the meantime (after proposal submission)
+					reply.setPerformative(ACLMessage.FAILURE);
+					reply.setContent("not-available");
+				}
+				myAgent.send(reply);
+			}
+			else {
+				block();
+			}
+		}
 	}
 
 }
